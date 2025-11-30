cb62413a74285224542ce31d03bba7ce91eba0dc
ElConejoDiablo <sven@seekspeakeasy.com>
2025-11-30 00:47:43 -0600

    Update mail-in label and allow vCard downloads

--- api/edge.js
+++ api/edge.js
@@ -16,6 +16,26 @@ module.exports = async function handler(req, res) {
     return res.end();
   }
 
+  const url = new URL(req.url);
+  const requestedPath = url.pathname;
+  if (requestedPath.toLowerCase().endsWith('.vcf')) {
+    const filename = requestedPath.replace(/^\//, '');
+    const filePath = path.join(__dirname, '..', filename);
+    try {
+      const contents = await fs.readFile(filePath);
+      res.writeHead(200, {
+        'Content-Type': 'text/vcard',
+        'Content-Disposition': `attachment; filename="${filename}"`,
+      });
+      res.end(contents);
+      return;
+    } catch (err) {
+      res.writeHead(404);
+      res.end('Not found');
+      return;
+    }
+  }
+
   const html = await loadIndex();
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
   res.end(html);
